cfe91777f2f7393e5d4090f15b82760041b132da
Saif Pathan <saifpathan363@gmail.com>
2021-03-31 08:56:53 +0530

    added src folder

---
+++ src/sum_of_two_digits.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	int b;
+	int c;
+	
+	printf("\nenter the value of a=");
+	scanf("%d",& a);
+	printf("\nenter the value of b=");
+	scanf("%d",& b);
+	
+	c=a+b;
+	printf("\nsum of a and b is=%d",c);
+	printf("\n\n");
+	
+}
